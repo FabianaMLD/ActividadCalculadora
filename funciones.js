a105d468533e8ebe854ee36dc318f0b50ccb066c
Fabiana Laree <fa.laree@duocuc.cl>
2021-05-04 13:00:29 -0400

    java4999440

--- funciones.js
+++ funciones.js
@@ -1,5 +1,22 @@
 function init(){
-    alert("Hola mundo!");
-    
+   
+    var resultado = document.getElementById("resultado");
+    var reseat = document.getElementById("reseat");
+    var suma = document.getElementById("suma");
+    var resta = document.getElementById("resta");
+    var multiplicacion = document.getElementById("multiplicacion");
+    var division = document.getElementById("division");
+    var igual = document.getElementById("igual");
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var cero = document.getElementById("cero");
+
 }
 
